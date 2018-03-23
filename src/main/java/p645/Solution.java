b98aaee5e154efae569e4c82fd9296407368e2e7
liuzhengyang <liuzhengyang@kuaishou.com>
2018-03-23 18:07:15 +0800

    p645

--- src/main/java/p645/Solution.java
+++ src/main/java/p645/Solution.java
@@ -6,21 +6,16 @@ package p645;
 public class Solution {
     public int[] findErrorNums(int[] nums) {
         int[] result = new int[2];
+        int expectedSum = (1 + nums.length) * nums.length / 2;
         int[] indicies = new int[nums.length];
+        int sum = 0;
         for (int num : nums) {
-            indicies[num-1] ++;
-        }
-        for (int i = 0; i < indicies.length; i++) {
-            if (result[0] > 0 && result[1] > 0) {
-                break;
-            }
-            if (indicies[i] > 1) {
-                result[0] = i + 1;
-            }
-            if (indicies[i] == 0) {
-                result[1] = i + 1;
+            sum += num;
+            if (++indicies[num-1]  > 1) {
+                result[0] = num;
             }
         }
+        result[1] = expectedSum - sum + result[0];
         return result;
     }
 }
