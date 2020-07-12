dcb0cc0613061bcfce4694a195bbd7cac3e5847a
liuzhengyang <liuzhengyang75@gmail.com>
2020-07-12 18:56:22 +0800

    add test

--- src/test/java/p5/SolutionTest.java
+++ src/test/java/p5/SolutionTest.java
@@ -6,6 +6,50 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SolutionTest {
+
+    // //[[1,1,0],
+    //// [1,1,0],
+    //// [0,0,1]]
+
+    @Test
+    public void testFriendCircle() {
+        int[][] nums = {{1,1,0}, {1,1,0}, {0,0,1}};
+        int length = nums.length;
+        visited = new int[length];
+        int max = 0;
+        for (int i = 0; i < length; i++) {
+            if (visited[i] != 0) {
+                continue;
+            }
+            max = Math.max(max, dfs(nums, i));
+        }
+        System.out.println(max);
+    }
+    public int findCircleNum(int[][] nums) {
+        int length = nums.length;
+        visited = new int[length];
+        int max = 0;
+        for (int i = 0; i < length; i++) {
+            if (visited[i] != 0) {
+                continue;
+            }
+            max = Math.max(max, dfs(nums, i));
+        }
+        return max;
+    }
+    static int[] visited;
+
+    int dfs(int[][] nums, int i) {
+        visited[i] = 1;
+        int result = 1;
+        for (int j = 0; j < nums[i].length; j++) {
+            if (visited[j] == 0 && nums[i][j] == 1) {
+                result += dfs(nums, j);
+            }
+        }
+        return result;
+    }
+
     @Test
     public void longestPalindrome() throws Exception {
     }
