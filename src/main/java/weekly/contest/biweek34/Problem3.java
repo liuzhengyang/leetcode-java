a20609c8258ae45950c6746d5d481d396475f9de
liuzhengyang <liuzhengyang75@gmail.com>
2020-09-09 15:06:41 +0800

    two pointer solution

--- src/main/java/weekly/contest/biweek34/Problem3.java
+++ src/main/java/weekly/contest/biweek34/Problem3.java
@@ -1,6 +1,7 @@
 package weekly.contest.biweek34;
 
 /**
+ * https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/
  * @author liuzhengyang
  */
 public class Problem3 {
@@ -10,10 +11,51 @@ public class Problem3 {
         System.out.println(problem3.findLengthOfShortestSubarray(new int[]{5,4,3,2,1}));
         System.out.println(problem3.findLengthOfShortestSubarray(new int[]{1,2,3}));
         System.out.println(problem3.findLengthOfShortestSubarray(new int[]{1}));
+        System.out.println(problem3.findLengthOfShortestSubarray(new int[]{3,2,1}));
         System.out.println(problem3.findLengthOfShortestSubarray(new int[]{10,13,17,21,15,15,9,17,22,22,13}));
     }
 
+    private int findLengthOfShortest(int[] array) {
+        if (array == null || array.length <= 1) {
+            return 0;
+        }
+        int l = 0;
+        int r = array.length - 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] >= array[i - 1]) {
+                l = i;
+            } else {
+                break;
+            }
+        }
+        for (int i = array.length - 2; i >= 0; i--) {
+            if (array[i] <= array[i + 1]) {
+                r = i;
+            } else {
+                break;
+            }
+        }
+        int maxLength = Math.max(l + 1, array.length - r);
+        int j = r;
+        if (maxLength == array.length) {
+            return 0;
+        }
+        for (int i = 0; i <= l; i++) {
+            while (j < array.length && array[j] < array[i]) {
+                j ++;
+                if (j == array.length) {
+                    break;
+                }
+            }
+            maxLength = Math.max(maxLength, i + 1 + (array.length - j));
+        }
+        return array.length - maxLength;
+    }
+
     public int findLengthOfShortestSubarray(int[] arr) {
+        if (true) {
+            return findLengthOfShortest(arr);
+        }
         if (arr == null || arr.length <= 1) {
             return 0;
         }
